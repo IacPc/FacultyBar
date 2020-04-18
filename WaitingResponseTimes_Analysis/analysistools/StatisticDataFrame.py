Corrected error in the confidence interval computation for the sample median

WaitingResponseTimes_Analysis/analysistools/StatisticDataFrame.py
@@ -200,8 +200,10 @@ class StatisticDataFrame:
         CI_upper_obs_number = math.ceil(obs_number*0.5 + standard_normal_quantile*math.sqrt(obs_number*0.5*0.5)) + 1
 
         sample_median = np.median(obs_vector)
-        CI_lower_bound = ordered_statistics[CI_lower_obs_number + 1]
-        CI_upper_bound = ordered_statistics[CI_upper_obs_number + 1]
+        # The confidence interval formula gives two indexes in the range 1..N,
+        # while the arrays have indexes in the range 0..N-1.
+        CI_lower_bound = ordered_statistics[CI_lower_obs_number - 1]
+        CI_upper_bound = ordered_statistics[CI_upper_obs_number - 1]
 
         return sample_median, sample_median-CI_lower_bound, CI_upper_bound-sample_median
 