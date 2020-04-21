04f515158db1f808954d8f3b8b0d2d5136855988
diegocasu <diegocasu95@libero.it>
2020-04-21 08:35:43 +0200

    Final polishing DimensioningSeatingNode

--- DimensioningSeatingNode/DimensioningSeatingNode.py
+++ DimensioningSeatingNode/DimensioningSeatingNode.py
@@ -23,9 +23,9 @@ def load_parameters():
         print("ERROR: the two lists number_of_seats and queue_size must have the same size")
         exit()
 
-    u = (vip_arrival_rate + normal_arrival_rate) / eating_rate  # utilization:scalar
-    utilization = np.full(fill_value=u, shape=len(number_of_seats)) / number_of_seats  # same as u but in list form
-    node_capacity = number_of_seats + queue_size    #scalar
+    u = (vip_arrival_rate + normal_arrival_rate) / eating_rate  # scalar
+    utilization = np.full(fill_value=u, shape=len(number_of_seats)) / number_of_seats  # utilization in list form
+    node_capacity = number_of_seats + queue_size  # scalar
 
     if len(utilization[utilization >= 1]) > 0:
         print("ERROR: One utilization is higher than one")
@@ -35,20 +35,18 @@ def load_parameters():
 
 
 def compute_formula_second_term(utilization, node_capacity, n_seats, u):
-    u_to_c_over_c_fact = math.fdiv(math.power(u, n_seats), math.factorial(n_seats))
-    p_to_K_minus_C_plus1 = math.power(utilization, (node_capacity - n_seats + 1))
+    u_to_n_seats_over_n_seats_fact = math.fdiv(math.power(u, n_seats), math.factorial(n_seats))
+    p_to_node_capacity_minus_n_seats_plus1 = math.power(utilization, (node_capacity - n_seats + 1))
     one_minus_p = math.fsub(1, utilization)
-    second_fraction_numerator = math.fsub(1, p_to_K_minus_C_plus1)
+    second_fraction_numerator = math.fsub(1, p_to_node_capacity_minus_n_seats_plus1)
     second_fraction = math.fdiv(second_fraction_numerator, one_minus_p)
-    return math.fmul(u_to_c_over_c_fact, second_fraction)
+    return math.fmul(u_to_n_seats_over_n_seats_fact, second_fraction)
 
-
-def realNumbSum(a, b): return math.fadd(a, b)
 
 def compute_p0(u, utilization, node_capacity, number_of_seats):
     factorial_sum = []
     for n_seats in number_of_seats:
-        temp_sum = math.mpf(0.0)  # type= real with arbitary precision
+        temp_sum = math.mpf(0.0)  # type = real with arbitary precision
         for j in range(0, n_seats):
             temp_sum = math.fadd(temp_sum, math.fdiv(math.power(u, j), math.factorial(j)))
         factorial_sum.append(temp_sum)
@@ -59,19 +57,19 @@ def compute_p0(u, utilization, node_capacity, number_of_seats):
         second_term.append(work)
 
     p0 = []
-    oneExtended = math.mpf(1.0)
+    one_extended = math.mpf(1.0)
     for i in range(0, len(factorial_sum)):
-        temp_real = realNumbSum(factorial_sum[i], second_term[i])
-        p0.append(math.fdiv(oneExtended, temp_real))
+        temp_real = math.fadd(factorial_sum[i], second_term[i])
+        p0.append(math.fdiv(one_extended, temp_real))
     return p0
 
 
 def compute_single_item_loss_list(u, node_capacity, n_seats, p0):
-    u_to_the_n = math.power(u, node_capacity)
-    C_fact = math.factorial(n_seats)
-    C_to_the_n_minus_c = math.power(n_seats, (node_capacity - n_seats))
-    denominator = math.fmul(C_fact, C_to_the_n_minus_c)
-    fraction = math.fdiv(u_to_the_n, denominator)
+    u_to_the_node_capacity = math.power(u, node_capacity)
+    n_seats_fact = math.factorial(n_seats)
+    n_seats_to_the_node_capacity_minus_n_seats = math.power(n_seats, (node_capacity - n_seats))
+    denominator = math.fmul(n_seats_fact, n_seats_to_the_node_capacity_minus_n_seats)
+    fraction = math.fdiv(u_to_the_node_capacity, denominator)
     return math.fmul(fraction, p0)
 
 
