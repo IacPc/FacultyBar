97de9fcf23510d5dcb662b1c33a453f94d095a5f
IacPc <iac.pacini@gmail.com>
2020-04-22 18:08:11 +0200

    added support for plotting loss probability behaviour varying seats and queue

--- PlotDimensioningSeatingNode/PlotDimensioningSeatingNode.py
+++ PlotDimensioningSeatingNode/PlotDimensioningSeatingNode.py
@@ -1,12 +1,12 @@
 import configparser as cp
-import texttable as tt
 import numpy as np
 import json
 import mpmath as math
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
 
-def convert_min_to_sec(value):
-    return value * 60
+def convert_min_to_sec(value): return value * 60
 
 
 def load_parameters():
@@ -16,37 +16,34 @@ def load_parameters():
     vip_arrival_rate = 1 / convert_min_to_sec(config["Customer"].getfloat("vip_interarrival_time"))
     normal_arrival_rate = 1 / convert_min_to_sec(config["Customer"].getfloat("normal_interarrival_time"))
     eating_rate = 1 / convert_min_to_sec(config["Customer"].getfloat("eating_time"))
-    number_of_seats = np.array(json.loads(config.get("SeatingNode", "number_of_seats")))
-    queue_size = np.array(json.loads(config.get("SeatingNode", "queue_size")))
+    number_of_seats = json.loads(config.get("SeatingNode", "number_of_seats"))  #min number of seats to have utilization <1
+    data_length = json.loads(config.get("SeatingNode", "data_length"))
+    u = (vip_arrival_rate + normal_arrival_rate) / eating_rate  # utilization:scalar
+    if u/number_of_seats >= 1:
+        print("ERROR: utilization greater or equal than 1")
+        exit(-1)
 
-    if len(number_of_seats) != len(queue_size):
-        print("ERROR: the two lists number_of_seats and queue_size must have the same size")
-        exit()
+    utilization = np.full(fill_value=u, shape=data_length) / number_of_seats  # same as u but in list form
 
-    u = (vip_arrival_rate + normal_arrival_rate) / eating_rate  # scalar
-    utilization = np.full(fill_value=u, shape=len(number_of_seats)) / number_of_seats  # utilization in list form
-    node_capacity = number_of_seats + queue_size  # scalar
-
-    if len(utilization[utilization >= 1]) > 0:
-        print("ERROR: One utilization is higher than one")
-        exit()
-
-    return u, utilization, node_capacity, number_of_seats, queue_size
+    return u, utilization, number_of_seats,data_length
 
 
 def compute_formula_second_term(utilization, node_capacity, n_seats, u):
-    u_to_n_seats_over_n_seats_fact = math.fdiv(math.power(u, n_seats), math.factorial(n_seats))
-    p_to_node_capacity_minus_n_seats_plus1 = math.power(utilization, (node_capacity - n_seats + 1))
+    u_to_c_over_c_fact = math.fdiv(math.power(u, n_seats), math.factorial(n_seats))
+    p_to_K_minus_C_plus1 = math.power(utilization, (node_capacity - n_seats + 1))
     one_minus_p = math.fsub(1, utilization)
-    second_fraction_numerator = math.fsub(1, p_to_node_capacity_minus_n_seats_plus1)
+    second_fraction_numerator = math.fsub(1, p_to_K_minus_C_plus1)
     second_fraction = math.fdiv(second_fraction_numerator, one_minus_p)
-    return math.fmul(u_to_n_seats_over_n_seats_fact, second_fraction)
+    return math.fmul(u_to_c_over_c_fact, second_fraction)
+
+
+def realNumbSum(a, b): return math.fadd(a, b)
 
 
 def compute_p0(u, utilization, node_capacity, number_of_seats):
     factorial_sum = []
     for n_seats in number_of_seats:
-        temp_sum = math.mpf(0.0)  # type = real with arbitary precision
+        temp_sum = math.mpf(0.0)  # type= real with arbitrary precision
         for j in range(0, n_seats):
             temp_sum = math.fadd(temp_sum, math.fdiv(math.power(u, j), math.factorial(j)))
         factorial_sum.append(temp_sum)
@@ -57,19 +54,19 @@ def compute_p0(u, utilization, node_capacity, number_of_seats):
         second_term.append(work)
 
     p0 = []
-    one_extended = math.mpf(1.0)
+    oneExtended = math.mpf(1.0)
     for i in range(0, len(factorial_sum)):
-        temp_real = math.fadd(factorial_sum[i], second_term[i])
-        p0.append(math.fdiv(one_extended, temp_real))
+        temp_real = realNumbSum(factorial_sum[i], second_term[i])
+        p0.append(math.fdiv(oneExtended, temp_real))
     return p0
 
 
 def compute_single_item_loss_list(u, node_capacity, n_seats, p0):
-    u_to_the_node_capacity = math.power(u, node_capacity)
-    n_seats_fact = math.factorial(n_seats)
-    n_seats_to_the_node_capacity_minus_n_seats = math.power(n_seats, (node_capacity - n_seats))
-    denominator = math.fmul(n_seats_fact, n_seats_to_the_node_capacity_minus_n_seats)
-    fraction = math.fdiv(u_to_the_node_capacity, denominator)
+    u_to_the_n = math.power(u, node_capacity)
+    C_fact = math.factorial(n_seats)
+    C_to_the_n_minus_c = math.power(n_seats, (node_capacity - n_seats))
+    denominator = math.fmul(C_fact, C_to_the_n_minus_c)
+    fraction = math.fdiv(u_to_the_n, denominator)
     return math.fmul(fraction, p0)
 
 
@@ -100,18 +97,16 @@ def plot_loss_probability(loss_probability_No_Queueing,loss_probability_with_que
 
 
 def main():
-    u, utilization, node_capacity, number_of_seats, queue_size = load_parameters()
-    loss_probability = compute_loss_probability(u, utilization, node_capacity, number_of_seats)
+    u, utilization, number_of_seats, data_length = load_parameters()
+    node_capacity_no_queue = np.arange(start=number_of_seats, stop=number_of_seats + data_length)
+    loss_probability_No_Queueing = compute_loss_probability(u, utilization, node_capacity_no_queue, node_capacity_no_queue)
 
-    result_table = tt.Texttable()
-    result_table.header(["Node capacity", "Number of seats", "Queue capacity", "Loss probability"])
-    result_table.set_cols_dtype(["t", "t", "t", "t"])
-
-    for row in zip(node_capacity, number_of_seats, queue_size, loss_probability):
-        result_table.add_row(row)
-
-    print(result_table.draw())
-
+    queue_size_not_null = np.arange(0, data_length)
+    number_of_seats_with_queue = np.full(fill_value=number_of_seats, shape=data_length)
+    node_capacity_with_queue = np.add(queue_size_not_null, number_of_seats_with_queue)
+    loss_probability_with_queue = compute_loss_probability(u, utilization, node_capacity_with_queue, number_of_seats_with_queue)
+    plot_loss_probability(loss_probability_No_Queueing, loss_probability_with_queue)
+   
 
 if __name__ == "__main__":
     main()
