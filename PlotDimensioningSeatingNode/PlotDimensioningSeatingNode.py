47b277cea5691a5b10521490c68bb7a26cf85ec4
diegocasu <diegocasu95@libero.it>
2020-04-25 20:50:18 +0200

    Polishing PlotDimensioningSeatingNode

--- PlotDimensioningSeatingNode/PlotDimensioningSeatingNode.py
+++ PlotDimensioningSeatingNode/PlotDimensioningSeatingNode.py
@@ -1,10 +1,11 @@
+from matplotlib.ticker import FormatStrFormatter
+import matplotlib.ticker as ticker
 import configparser as cp
 import numpy as np
 import json
 import mpmath as math
 import matplotlib.pyplot as plt
 import texttable as tt
-from matplotlib.ticker import FormatStrFormatter
 
 
 def convert_min_to_sec(value): return value * 60
@@ -17,16 +18,12 @@ def load_parameters():
     vip_arrival_rate = 1 / convert_min_to_sec(config["Customer"].getfloat("vip_interarrival_time"))
     normal_arrival_rate = 1 / convert_min_to_sec(config["Customer"].getfloat("normal_interarrival_time"))
     eating_rate = 1 / convert_min_to_sec(config["Customer"].getfloat("eating_time"))
-    number_of_seats = json.loads(config.get("SeatingNode", "number_of_seats"))  #min number of seats to have utilization <1
+    number_of_seats = json.loads(config.get("SeatingNode", "number_of_seats"))
     data_length = json.loads(config.get("SeatingNode", "data_length"))
-    u = (vip_arrival_rate + normal_arrival_rate) / eating_rate  # utilization:scalar
-    if u/number_of_seats >= 1:
-        print("ERROR: utilization greater or equal than 1")
-        exit(-1)
+    u = (vip_arrival_rate + normal_arrival_rate) / eating_rate  # scalar
+    utilization = np.full(fill_value=u, shape=data_length) / number_of_seats  # utilization but in list form
 
-    utilization = np.full(fill_value=u, shape=data_length) / number_of_seats  # same as u but in list form
-
-    return u, utilization, number_of_seats,data_length
+    return u, utilization, number_of_seats, data_length
 
 
 def compute_formula_second_term(utilization, node_capacity, n_seats, u):
@@ -36,9 +33,6 @@ def compute_formula_second_term(utilization, node_capacity, n_seats, u):
     second_fraction_numerator = math.fsub(1, p_to_K_minus_C_plus1)
     second_fraction = math.fdiv(second_fraction_numerator, one_minus_p)
     return math.fmul(u_to_c_over_c_fact, second_fraction)
-
-
-def realNumbSum(a, b): return math.fadd(a, b)
 
 
 def compute_p0(u, utilization, node_capacity, number_of_seats):
@@ -57,7 +51,7 @@ def compute_p0(u, utilization, node_capacity, number_of_seats):
     p0 = []
     oneExtended = math.mpf(1.0)
     for i in range(0, len(factorial_sum)):
-        temp_real = realNumbSum(factorial_sum[i], second_term[i])
+        temp_real = math.fadd(factorial_sum[i], second_term[i])
         p0.append(math.fdiv(oneExtended, temp_real))
     return p0
 
@@ -82,16 +76,26 @@ def compute_loss_probability(u, utilization, node_capacity, number_of_seats):
     return loss_probability
 
 
-def plot_loss_probability(loss_probability_No_Queueing,loss_probability_with_queue):
+def plot_loss_probability(loss_probability_No_Queueing, loss_probability_with_queue):
+    figure = plt.figure(figsize=(13.66, 7.68))
     plot_axes = plt.gca()
-    plot_axes.set_ylabel("Loss Probability", fontsize=14, labelpad=10, rotation=90)
+    plot_axes.set_ylabel(r'$p_{LOSS}$', fontsize=14, labelpad=10, rotation=90)
+    plot_axes.set_xlabel(r'Capacity $K$', fontsize=14, labelpad=10)
     plot_axes.yaxis.set_major_formatter(FormatStrFormatter('%.3f'))
 
-    x_values = np.arange(start=0, stop=len(loss_probability_No_Queueing), step=1.0)
+    x_values = np.arange(start=6, stop=len(loss_probability_No_Queueing)+6, step=1.0)
 
-    plot_axes.plot(x_values, loss_probability_No_Queueing, marker="", lw=2, label="More Seat")
-    plot_axes.plot(x_values, loss_probability_with_queue, marker="", lw=2, label="More Queue")
+    plot_axes.plot(x_values, loss_probability_No_Queueing, marker="", lw=2, color="cornflowerblue", label="Increasing only " + r'$N_{SEAT}$')
+    plot_axes.plot(x_values, loss_probability_with_queue, marker="", lw=2, color="darkorange", label="Increasing only " + r'$N_{q, SEATING}$')
+    plot_axes.plot(np.arange(start=0, stop=50), np.full(fill_value=0.01, shape=50), marker="", lw=1, linestyle="--", color="black", label=r'$p_{LOSS}=1$%')
 
+    plot_axes.set_xlim([6, 30])
+    plot_axes.set_ylim([0, 0.100])
+
+    plot_axes.xaxis.set_major_locator(ticker.MultipleLocator(base=1.0))
+    plot_axes.yaxis.set_major_locator(ticker.MultipleLocator(base=0.01))
+
+    plt.margins(0)
     plt.legend(loc="upper right", prop={'size': 14})
     plt.draw()
     plt.show(block=True)
@@ -106,7 +110,7 @@ def main():
     number_of_seats_with_queue = np.full(fill_value=number_of_seats, shape=data_length)
     node_capacity_with_queue = np.add(queue_size_not_null, number_of_seats_with_queue)
     loss_probability_with_queue = compute_loss_probability(u, utilization, node_capacity_with_queue, number_of_seats_with_queue)
-    # plot_loss_probability(loss_probability_No_Queueing, loss_probability_with_queue)
+    plot_loss_probability(loss_probability_No_Queueing, loss_probability_with_queue)
     
     result_table_more_seats = tt.Texttable()
     result_table_more_seats.header(["Node capacity", "Number of seats", "Queue capacity", "Loss probability"])
@@ -120,7 +124,7 @@ def main():
     result_table_more_queue = tt.Texttable()
     result_table_more_queue.header(["Node capacity", "Number of seats", "Queue capacity", "Loss probability"])
     result_table_more_queue.set_cols_dtype(["t", "t", "t", "t"])
-    for row in zip(node_capacity_with_queue, number_of_seats_with_queue, node_capacity_with_queue, loss_probability_with_queue):
+    for row in zip(node_capacity_with_queue, number_of_seats_with_queue, queue_size_not_null, loss_probability_with_queue):
         result_table_more_queue.add_row(row)
 
     print("************* INCREASING QUEUE LENGTH *************")
